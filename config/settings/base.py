fa342128f6f403c6f8c5024087b3aec60e6056ed
Kamilgolda <kamil.goldaat@gmail.com>
2021-04-21 23:22:37 +0200

    Email_Backend
    Dodałem maila dla naszej aplikacji.
    Po rejestracji jest wysyłany mail w celu potwierdzenia założenia konta.
    Login: projektzespolowyitflies@gmail.com
    Hasło: zaq1@WSX

--- config/settings/base.py
+++ config/settings/base.py
@@ -215,9 +215,15 @@ X_FRAME_OPTIONS = "DENY"
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-EMAIL_BACKEND = env(
+'''EMAIL_BACKEND = env(
     "DJANGO_EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
-)
+)'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'projektzespolowyitflies@gmail.com'
+EMAIL_HOST_PASSWORD = 'zaq1@WSX'
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-timeout
 EMAIL_TIMEOUT = 5
 
