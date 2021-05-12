e40c9bf80c0f59a215ac0c8162172cc69128e062
Kamilgolda <kamil.goldaat@gmail.com>
2021-05-12 08:34:12 +0200

    #49 Dodanie wycieczek i terminów
    dodatkowo eksport wycieczek i rezerwacji do pliku xlsx

---
+++ travels/management/commands/add_dates.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os.path
+from django.core.management.base import BaseCommand, CommandError
+import logging
+from travels.models import Trip, TripDates
+import datetime
+import random
+
+
+class Command(BaseCommand):
+    help = "Creating Trips Dates"
+
+    trips = Trip.objects.all()
+
+    def handle(self, *args, **options):
+        for trip in self.trips:
+            terms = TripDates.objects.all().filter(trip=trip)
+            no = len(terms)
+            if no == 0:
+                _, created = TripDates.objects.get_or_create(trip=trip,
+                                                             start_date=datetime.datetime(2020, 5, random.randint(1, 31))
+                                                             )
+                if created:
+                    logging.info("Dodano 1 Termin dla wycieczki ", trip)
+
+                _, created = TripDates.objects.get_or_create(trip=trip,
+                                                             start_date=datetime.datetime(2020, 6,
+                                                                                          random.randint(1, 30))
+                                                             )
+                if created:
+                    logging.info("Dodano 2 Termin dla wycieczki ", trip)
+
+                _, created = TripDates.objects.get_or_create(trip=trip,
+                                                             start_date=datetime.datetime(2020, 7,
+                                                                                          random.randint(1, 31))
+                                                             )
+                if created:
+                    logging.info("Dodano 3 Termin dla wycieczki ", trip)
